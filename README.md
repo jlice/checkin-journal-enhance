3ba3b9abec56315107ef6cd2308a33efe9fbf200
jlice <1121672253@qq.com>
2018-08-01 19:15:24 +0800

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# shanbay-user-scripts
+
+用于扇贝网的 Tampermonkey（油猴）脚本。
---
+++ journal-enhancement/user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         打卡日记增强
+// @namespace    https://www.shanbay.com/
+// @version      0.1.1
+// @description  打卡日记增强
+// @author       文剑木然
+// @match        https://www.shanbay.com/checkin/user/*
+// @require      https://static.baydn.com/static/scripts/jquery-1.7.2.min.js
+// @updateURL   https://raw.githubusercontent.com/jlice/shanbay-user-scripts/master/journal-enhancement/meta.js
+// @downloadURL https://raw.githubusercontent.com/jlice/shanbay-user-scripts/master/journal-enhancement/user.js
+// ==/UserScript==
