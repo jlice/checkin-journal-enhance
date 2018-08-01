e68657f31f95d8f87292e4b5427ce50716ddba75
jlice <1121672253@qq.com>
2018-08-01 19:24:36 +0800

    update

--- journal-enhancement/user.js
+++ journal-enhancement/user.js
@@ -1,11 +1,13 @@
 // ==UserScript==
 // @name         打卡日记增强
-// @namespace    https://www.shanbay.com/
-// @version      0.1.1
+// @namespace    https://github.com/jlice/shanbay-user-scripts
+// @supportURL   https://github.com/jlice/shanbay-user-scripts
+// @version      0.1.2
 // @description  打卡日记增强
-// @author       文剑木然
-// @match        https://www.shanbay.com/checkin/user/*
+// @author       文剑木然 <1121672253@qq.com>
+// @match        *://www.shanbay.com/checkin/user/*
+// @run-at       document-end
 // @require      https://static.baydn.com/static/scripts/jquery-1.7.2.min.js
-// @updateURL   https://raw.githubusercontent.com/jlice/shanbay-user-scripts/master/journal-enhancement/meta.js
-// @downloadURL https://raw.githubusercontent.com/jlice/shanbay-user-scripts/master/journal-enhancement/user.js
+// @updateURL    https://raw.githubusercontent.com/jlice/shanbay-user-scripts/master/journal-enhancement/meta.js
+// @downloadURL  https://raw.githubusercontent.com/jlice/shanbay-user-scripts/master/journal-enhancement/user.js
 // ==/UserScript==
